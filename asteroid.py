a80884aefc84352aac934b3341664bf09936a2bf
Brayan Perez <brayanperezdev@gmail.com>
2026-04-28 00:05:49 -0500

    fate: finish the game know the asteroids crash and split!

--- asteroid.py
+++ asteroid.py
@@ -1,6 +1,8 @@
 from circleshape import CircleShape
-from constants import LINE_WIDTH
+from constants import LINE_WIDTH, ASTEROID_MIN_RADIUS
 import pygame
+from logger import log_event
+import random
 
 
 class Asteroid(CircleShape):
@@ -12,3 +14,19 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += self.velocity * dt
+
+    def split(self):
+        self.kill()
+
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+
+        log_event("asteroid_split")
+        ranodm_angle = random.uniform(20, 50)
+        first_vector_change = self.velocity.rotate(ranodm_angle)
+        second_vector_change = self.velocity.rotate(-ranodm_angle)
+        self.radius = self.radius - ASTEROID_MIN_RADIUS
+        asteroid1 = Asteroid(self.position, self.position, self.radius)
+        asteroid2 = Asteroid(self.position, self.position, self.radius)
+        asteroid1.velocity = first_vector_change * 1.2
+        asteroid2.velocity = second_vector_change * 1.2
